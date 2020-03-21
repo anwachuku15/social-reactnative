9fc7e3642c836d42855ce9c9ee7140da373bfad5
anwachuku15 <anwachuku15@gmail.com>
2020-03-21 18:58:52 -0400

    gitignored firebase key

--- App.js
+++ App.js
@@ -6,18 +6,10 @@ import Home from './screens/Home'
 import Login from './screens/Login'
 import Register from './screens/Register'
 
+import * as config from './config'
 import * as firebase from 'firebase'
 
-var config = {
-  apiKey: "AIzaSyCH2ZZ37WAZmH4WMiZLreRdI7DGomWyowE",
-  authDomain: "social-reactnative.firebaseapp.com",
-  databaseURL: "https://social-reactnative.firebaseio.com",
-  projectId: "social-reactnative",
-  storageBucket: "social-reactnative.appspot.com",
-  messagingSenderId: "577927858292",
-  appId: "1:577927858292:web:6196bddee66b3d4393a921",
-  measurementId: "G-078XY3CM9R"
-};
+
 // Initialize Firebase -- go back to figure out why !firebase.apps.length
 if(!firebase.apps.length){
   firebase.initializeApp(config);
